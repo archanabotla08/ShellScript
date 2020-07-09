d76c4ff905f359ad2e676ff274ecc5bd910882fd
Nisha271 <bukkenisha@gmail.com>
2020-07-09 22:48:57 +0530

    Newly created UC-5-EmployeeWage_ForMonthUC branch and calcuated the Employee Wage for Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,18 +2,24 @@
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
-randomCheck=$(( RANDOM%3 ))
+totalSalary=0;
+numWorkingDays=20
 
-case $randomCheck in
-        $isFullTime )
-                empHrs=8
-                ;;
-        $isPartTime )
-                empHrs=4
-                ;;
-        * )
-                empHrs=0
-                ;;
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+        empCheck=$(( RANDOM%3 ));
+        case $empCheck in
+                $isFullTime )
+                        empHrs=8
+                        ;;
+                $isPartTime )
+                        empHrs=4
+                        ;;
+                * )
+                        empHrs=0
+                        ;;
         esac
-salary=$(( $empRatePerHr * $empHrs ))
+        salary=$(( $empHrs*$empRatePerHr ))
+        totalSalary=$(( $totalSalary+$salary ))
+done
 
