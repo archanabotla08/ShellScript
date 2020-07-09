1ca9a86b89b68766f092c5f6df2600a2864532ff
Nisha271 <bukkenisha@gmail.com>
2020-07-09 22:37:49 +0530

    newely Created UC-4-employeeWage_FullTime_PartTime_Using_Switch-CaseUC and calculate Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,15 +4,16 @@ isFullTime=2;
 empRatePerHr=20;
 randomCheck=$(( RANDOM%3 ))
 
-if [ $isFullTime -eq $randomCheck ]
-then
-        empHrs=8;
-elif [ $isPartTime -eq $randomCheck ]
-then
-        empHrs=4;
-else
-        empHrs=0;
-fi
-salary=$(( $empRatePerHr * $empHrs))
-echo "Employee Wages $salary"
+case $randomCheck in
+        $isFullTime )
+                empHrs=8
+                ;;
+        $isPartTime )
+                empHrs=4
+                ;;
+        * )
+                empHrs=0
+                ;;
+        esac
+salary=$(( $empRatePerHr * $empHrs ))
 
